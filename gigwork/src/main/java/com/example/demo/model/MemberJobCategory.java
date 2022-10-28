b2fbad798565553017e936960368cc49cfb334b7
SUNGJUN1234 <chotjdwns654@naver.com>
2022-10-28 12:51:00 +0900

    모델진짜완성/성준

--- gigwork/src/main/java/com/example/demo/model/MemberJobCategory.java
+++ gigwork/src/main/java/com/example/demo/model/MemberJobCategory.java
@@ -3,7 +3,7 @@ package com.example.demo.model;
 import lombok.Data;
 
 @Data
-public class Member_job_category {
+public class MemberJobCategory {
 	private int mc_seq;
 	private int category_seq;
 	private String mem_id;
