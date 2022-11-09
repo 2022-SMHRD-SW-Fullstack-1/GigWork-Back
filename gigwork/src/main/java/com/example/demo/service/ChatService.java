dbe342c2f945088d946b5db40573da8343868370
Brinicle177 <kimkr0421@naver.com>
2022-11-09 11:24:02 +0900

    채팅방 개설/설아

--- gigwork/src/main/java/com/example/demo/service/ChatService.java
+++ gigwork/src/main/java/com/example/demo/service/ChatService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.example.demo.mapper.ChatMapper;
 import com.example.demo.model.ChattingContent;
@@ -28,6 +29,14 @@ public class ChatService {
 	public void putChatContent(Map<String, String> data) {
 		ChattingContent chatContent = new ChattingContent(0, data.get("talker"), data.get("msg"), data.get("msg_time"), Integer.parseInt(data.get("cr_seq")));
 		chatMapper.putChatContent(chatContent);
+	}
+	
+	public void createChatRoom(@RequestBody Map<String, String> data) {
+		ChattingRoom chatroom = new ChattingRoom();
+		chatroom.setMem_nick(data.get("mem_nick"));
+		chatroom.setPartner_nick(data.get("partner_nick"));
+		chatroom.setPost_num(Integer.parseInt(data.get("post_num")));
+		chatMapper.createChatRoom(chatroom);
 	}
 	
 
