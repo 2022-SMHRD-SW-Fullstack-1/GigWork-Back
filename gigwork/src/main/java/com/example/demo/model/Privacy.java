a74c8048f1e389eaa6a55d2da8d87d5038220599
SUNGJUN1234 <chotjdwns654@naver.com>
2022-11-10 16:03:45 +0900

    프라이버시모델/성준

---
+++ gigwork/src/main/java/com/example/demo/model/Privacy.java
@@ -0,0 +1,13 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Privacy {
+	private String img_src;
+	private String name;
+	private String mem_id;
+	private String mem_phone;
+}
